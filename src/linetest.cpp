72736c5c5c4409a5741642ed2cd5a4b58e8859ba
Tommy <gonghsunchen@gmail.com>
2025-10-18 17:23:37 +0800

    update

--- src/linetest.cpp
+++ src/linetest.cpp
@@ -5,8 +5,12 @@ const int Pin = 41;
 const int Pin1 = 40;    
 const int Pin2 = 39;   
 
-volatile bool touch = false; 
+volatile bool centerTouch = false;
+volatile bool leftTouch   = false;
+volatile bool rightTouch  = false;
 void stop();
+void stop1();
+void stop2();
 
 int val = 0;  // var
 int val1 = 0;  // var
@@ -49,25 +53,45 @@ void loop(){
     Serial.println("Pin2");         // debug value
     Serial.println(digitalRead(Pin2)); 
     delay(1000);*/
-
-    if(touch){
-        Serial.println("stop");
-        Vector_Motion(0,0);
+    readBNO085Yaw();
+    if(centerTouch==true){
+        Serial.println("back");
+        Vector_Motion(0,30);  
+        if(digitalRead(Pin) == 0){
+        Vector_Motion(0,0);          
+            centerTouch = false;    
+        }
+            
+    }
+    else if(leftTouch==true){
+        Serial.println("right");
+        Vector_Motion(30,0);  
+        if(digitalRead(Pin1) == 0){
+            Vector_Motion(0,0);        
+            leftTouch = false;
+    }
+    }
+    else if(rightTouch==true){
+        Serial.println("left");
+        Vector_Motion(-30,0); 
+        if(digitalRead(Pin2) == 0){
+        Vector_Motion(0,0);       
+            rightTouch = false;   
+        }
+            
     }
     else{
-        Serial.println("running");
-        Vector_Motion(30,0);
-    }
-       
-    
+        Serial.println("RUNNING");
+        Vector_Motion(0,0);   
+    }    
     
 }   
 void stop(){
-    touch = true;
+    centerTouch = true;
 }
 void stop1(){
-    LEFTTOUCH = true;
+    leftTouch = true;
 }
 void stop2(){
-    touch = true;
+    rightTouch = true;
 }
